3fb59ae6ffedcff53c28a4df642bee19d33fa80c
Luke Ward <lwarddev@gmail.com>
2025-08-11 23:32:40 +0100

    Added Login and Sign up pages

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db/drizzle"; // your drizzle instance
+import { schema } from "@/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "pg", // or "mysql", "sqlite"
+    provider: "pg",
+    schema,
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
 });
